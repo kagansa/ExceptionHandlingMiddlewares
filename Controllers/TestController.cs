0c3fd8e8c306f58a3ecd94b2293557c83cbbecda
Kağan <kagansaygin@gmail.com>
2022-08-28 13:35:30 +0300

    Initial commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExceptionHandlingMiddlewares.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("ex")]
+        public ActionResult ex()
+        {
+            try
+            {
+                int n1 = 10;
+                int n2 = 0;
+                int result = n1/n2;
+                return Ok(result);
+            }
+            catch (Exception e)
+            { 
+               return BadRequest("HATA ! "+e.Message);
+            }
+        }
+
+        [HttpGet("ex2")]
+        public ActionResult ex2()
+        {
+            int n1 = 10;
+            int n2 = 0;
+            int result = n1/n2;
+            return Ok(result);
+        }
+    }
+}
---
+++ Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace ExceptionHandlingMiddlewares.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _request;
+
+        public ExceptionHandlerMiddleware(RequestDelegate request)
+        {
+            _request = request;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _request.Invoke(httpContext);
+            }
+            catch (Exception ex)
+            {
+                //İşlemler
+                
+            }
+        }
+    }
+}
